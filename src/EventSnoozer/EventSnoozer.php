4eb79ed67582cc3e2327660c4f81b23a29104227
Evgeny Vyatkin <vyatkinevgeny@gmail.com>
2017-03-28 20:22:37 +0300

    Implemented EventSnoozer functionality

--- src/EventSnoozer/EventSnoozer.php
+++ src/EventSnoozer/EventSnoozer.php
@@ -4,6 +4,7 @@ namespace EventSnoozer;
 
 use EventSnoozer\EventSnoozer\EventStorageInterface;
 use EventSnoozer\StoredEvent\StoredEvent;
+use EventSnoozer\StoredEvent\StoredEventInterface;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
@@ -30,29 +31,82 @@ class EventSnoozer
     }
 
     /**
-     * @param Event $event
+     * @param string $eventName
+     * @param RealEvent $event
      * @param string $snoozeTime
-     * @param int $priority
-     * @param array $additionalData
      * @return bool
      */
-    public function snoozeEvent(Event $event, $snoozeTime = '+1 min', $priority = 0, array $additionalData = array())
+    public function snoozeEvent($eventName, RealEvent $event, $snoozeTime = '+1 min')
     {
         $storedEvent = new StoredEvent();
         $storedEvent->setEventClass(get_class($event))
+            ->setEventName($eventName)
             ->setRuntime(new \DateTime($snoozeTime))
-            ->setPriority($priority)
-            ->setAdditionalData($additionalData);
+            ->setPriority($event->getPriority())
+            ->setAdditionalData($event->getAdditionalData());
 
         return $this->eventStorage->saveEvent($storedEvent);
     }
 
     /**
+     * @return bool
+     */
+    public function dispatchSnoozedEvent()
+    {
+        $event = $this->eventStorage->fetchEvent();
+
+        if ($event instanceof StoredEventInterface) {
+            if (class_exists($event->getEventClass())) {
+                $className = $event->getEventClass();
+                $realEvent = new $className();
+                if ($realEvent instanceof RealEvent) {
+                    $realEvent->setPriority($event->getPriority())
+                        ->setAdditionalData($event->getAdditionalData());
+                }
+
+                $this->dispatchEvent($event->getEventName(), $realEvent);
+            }
+
+            $this->eventStorage->removeEvent($event);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param int $count
+     * @return int
+     */
+    public function dispatchMultipleSnoozedEvents($count = 1)
+    {
+        $events = $this->eventStorage->fetchMultipleEvents($count);
+
+        $dispatched = 0;
+        foreach ($events as $event) {
+            if (class_exists($event->getEventClass())) {
+                $className = $event->getEventClass();
+                $realEvent = new $className();
+                if ($realEvent instanceof RealEvent) {
+                    $realEvent->setPriority($event->getPriority())
+                        ->setAdditionalData($event->getAdditionalData());
+                }
+
+                $this->dispatchEvent($event->getEventName(), $realEvent);
+                $dispatched++;
+            }
+            $this->eventStorage->removeEvent($event);
+        }
+
+        return $dispatched;
+    }
+
+    /**
+     * @param string $eventName
      * @param Event $event
      * @return Event
      */
-    public function dispatchEvent(Event $event)
+    public function dispatchEvent($eventName, Event $event)
     {
-        return $this->eventDispatcher->dispatch($event);
+        return $this->eventDispatcher->dispatch($eventName, $event);
     }
 }
