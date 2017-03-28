9e59618c98e7d4495e4730fb8c7d42cc00175c06
Evgeny Vyatkin <vyatkinevgeny@gmail.com>
2017-03-28 20:17:16 +0300

    Created RealEvent for storing some additional data and priority

---
+++ src/EventSnoozer/RealEvent.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace EventSnoozer;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class RealEvent extends Event
+{
+    /**
+     * @var int
+     */
+    protected $priority;
+
+    /**
+     * @var array
+     */
+    protected $additionalData;
+
+    /**
+     * @return int
+     */
+    public function getPriority()
+    {
+        return $this->priority ?: 0;
+    }
+
+    /**
+     * @param int $priority
+     * @return RealEvent
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAdditionalData()
+    {
+        return is_array($this->additionalData) ? $this->additionalData : array();
+    }
+
+    /**
+     * @param array $additionalData
+     */
+    public function setAdditionalData(array $additionalData)
+    {
+        $this->additionalData = $additionalData;
+    }
+}
