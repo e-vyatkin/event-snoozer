09d1b4bf62ad9f81f50a2fd785e9c34a64285c3c
Evgeny Vyatkin <vyatkinevgeny@gmail.com>
2017-03-28 19:40:14 +0300

    Defined required methods in StoredEventInterface and implemented in StoredEvent

--- src/EventSnoozer/StoredEvent/StoredEvent.php
+++ src/EventSnoozer/StoredEvent/StoredEvent.php
@@ -10,6 +10,31 @@ class StoredEvent implements StoredEventInterface
     protected $runtime;
 
     /**
+     * @var string
+     */
+    protected $className;
+
+    /**
+     * @var int
+     */
+    protected $priority;
+
+    /**
+     * @var array
+     */
+    protected $additionalData;
+
+    /**
+     * StoredEvent constructor.
+     */
+    public function __construct()
+    {
+        $this->priority = 0;
+        $this->runtime = new \DateTime();
+        $this->additionalData = [];
+    }
+
+    /**
      * @param \DateTime $runtime
      * @return StoredEventInterface
      */
@@ -26,5 +51,62 @@ class StoredEvent implements StoredEventInterface
     public function getRuntime()
     {
         return $this->runtime instanceof \DateTime ? $this->runtime : new \DateTime();
+    }
+
+    /**
+     * @param string $className
+     * @return StoredEventInterface
+     */
+    public function setEventClass($className)
+    {
+        $this->className = $className;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventClass()
+    {
+        return $this->className;
+    }
+
+    /**
+     * @param int $priority
+     * @return StoredEventInterface
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @param array $additionalData
+     * @return StoredEventInterface
+     */
+    public function setAdditionalData(array $additionalData)
+    {
+        $this->additionalData = $additionalData;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAdditionalData()
+    {
+        return $this->additionalData;
     }
 }
--- src/EventSnoozer/StoredEvent/StoredEventInterface.php
+++ src/EventSnoozer/StoredEvent/StoredEventInterface.php
@@ -14,4 +14,37 @@ interface StoredEventInterface
      * @return \DateTime
      */
     public function getRuntime();
+
+    /**
+     * @param string $className
+     * @return StoredEventInterface
+     */
+    public function setEventClass($className);
+
+    /**
+     * @return string
+     */
+    public function getEventClass();
+
+    /**
+     * @param int $priority
+     * @return StoredEventInterface
+     */
+    public function setPriority($priority);
+
+    /**
+     * @return int
+     */
+    public function getPriority();
+
+    /**
+     * @param array $additionalData
+     * @return StoredEventInterface
+     */
+    public function setAdditionalData(array $additionalData);
+
+    /**
+     * @return array
+     */
+    public function getAdditionalData();
 }
